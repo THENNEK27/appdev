a3d9d4800e988ac99e7451495984fc5f0da83b45
THENNEK27 <132435666+THENNEK27@users.noreply.github.com>
2023-05-09 00:12:04 +0800

    Add files via upload

---
+++ KennethMontero/main.js
@@ -0,0 +1,48 @@
+var words = ["COMSI", "STUDENT", "MSC"]
+
+var counter = 0;
+var currentIndex = getRandomInt(0,words.length -1);
+var text = document.querySelector("#type-it");
+
+var stepInterval = setInterval(() =>{step();},200);
+var delInterval = null;
+var delTimeout = null;
+
+function getRandomInt(min,max){
+    min = Math.ceil(min)
+    maxx = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function delIntervalCallback(){
+    delInterval = setInterval(() => {del();},100);
+    clearTimeout(delTimeout);
+}
+function del(){
+    if(counter == 0){
+        let newIndex = getRandomInt(0,words.length -1);
+        while(newIndex == currentIndex ) {
+            newIndex =getRandomInt(0,words.length -1);
+        }
+        currentIndex = newIndex;
+        clearInterval(delInterval);
+        stepInterval = setInterval(() => {step();}, 200 );
+    }
+    else{
+        text.textContent = text.textContent.slice(0,-1);
+        counter--;
+    }
+}
+
+function step(){
+    if(counter >= words [currentIndex].length){
+        clearInterval(stepInterval);
+        delTimeout = setTimeout(()=>{delIntervalCallback();},200 );
+    }
+    else{
+        text.textContent += words [currentIndex][counter];
+        counter++;
+    }
+
+}
+
